9eb873e4c7d71dd553a7e0c8eb6f27ae7d154a3d
Uri Shilo <uris@juganu.com>
2020-09-27 23:34:57 +0300

    excelent explanation on batch files in folder Lesson_22

---
+++ Lesson_22/hello.bat
@@ -0,0 +1,2 @@
+@py C:\Users\DELL\Projects\Playground\AutomateTheBoringStuffWithPython\Lesson_22\hello.py %*
+@pause
---
+++ Lesson_22/hello.py
@@ -0,0 +1,28 @@
+#! python3
+import sys
+
+print('Hello world!')
+print(sys.argv)
+
+# py.exe hello.py
+
+# cmd + r
+# py.exe C:\Users\DELL\Projects\Playground\AutomateTheBoringStuffWithPython\Lesson_22\hello.py
+
+# cmd + r
+# py C:\Users\DELL\Projects\Playground\AutomateTheBoringStuffWithPython\Lesson_22\hello.py
+
+# cmd + r
+# py C:\Users\DELL\Projects\Playground\AutomateTheBoringStuffWithPython\Lesson_22\hello.py
+
+
+# pause.exe
+
+
+# C:\Users\DELL\Projects\Playground\AutomateTheBoringStuffWithPython\Lesson_22\hello.bat
+# C:\Users\DELL\Projects\Playground\AutomateTheBoringStuffWithPython\Lesson_22\hello
+
+# define folder in the path variables and then
+# cmd + r
+# hello
+
