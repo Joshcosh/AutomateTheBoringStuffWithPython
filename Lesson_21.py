afa5ea26615315601f423997b801d3cda7d5fb2b
Uri Shilo <uris@juganu.com>
2020-09-27 22:26:39 +0300

    just finished lesson 21

--- Lesson_21.py
+++ Lesson_21.py
@@ -6,3 +6,20 @@ name = 'Alice'
 place = 'Main Street'
 time = '6 pm'
 food = 'turnips'
+
+spam = 'Hello ' + name + ', you are invited to a patry at ' + place + \
+' at ' + time + '. Please bring ' + food + '.'
+
+print(spam)
+# %%
+# 2 | do it better with string formatting
+name = 'Alice'
+place = 'Main Street'
+time = '6 pm'
+food = 'turnips'
+
+spam = '''Hello %s,
+You are invited to a party at %s at %s.
+Please bring %s''' % (name, place, time, food)
+
+print(spam)
